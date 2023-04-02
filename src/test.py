336e761b0ae8c0c45d498034b135ea39c4b78877
Matúš Hubinský <matus.hubinsky@scot.sk>
2023-04-02 12:46:37 +0200

    style: doxygen commenst for each test

--- src/test.py
+++ src/test.py
@@ -18,7 +18,10 @@ import mathlib
 
 
 # TODO: test for priority
-def test_input_process() -> None:
+# @brief tests for the whole module calculate
+# @param
+# @return
+def test_calculate() -> None:
 	assert core.calculate("1+2") == 3
 	assert core.calculate("1/2") == 0.5
 	assert core.calculate("1000+2000") == 3000
@@ -26,6 +29,9 @@ def test_input_process() -> None:
 	assert core.calculate("2^10+1") == 1025
 
 
+# @brief tests for function add() from mathlib
+# @param
+# @return
 def test_addition() -> None:
 	assert mathlib.add(None, 0, 0) == 0
 	assert mathlib.add(None, 1, 2) == 3
@@ -33,6 +39,9 @@ def test_addition() -> None:
 	assert mathlib.add(None, 12345678901, 1) == 12345678902
 
 
+# @brief tests for function sub() from mathlib
+# @param
+# @return
 def test_substraction() -> None:
 	assert mathlib.sub(None, 0, 0) == 0
 	assert mathlib.sub(None, 1, 2) == -1
@@ -42,6 +51,9 @@ def test_substraction() -> None:
 	assert mathlib.sub(None, 12345678901, 1) == 12345678900
 
 
+# @brief tests for function mul() from mathlib
+# @param
+# @return
 def test_multiplication() -> None:
 	assert mathlib.mul(None, 0, 0) == 0
 	assert mathlib.mul(None, 1, 1) == 1
@@ -50,6 +62,9 @@ def test_multiplication() -> None:
 	assert mathlib.mul(None, 12345678901, 1) == 12345678901
  
 
+# @brief tests for function div() from mathlib, divison by zero will return error
+# @param
+# @return
 def test_divison() -> None:
 	with pytest.raises(ZeroDivisionError):
 		mathlib.div(None, 1, 0) == 0
@@ -59,6 +74,9 @@ def test_divison() -> None:
 	assert mathlib.div(None, 12345678901, 1) == 12345678901
 
 
+# @brief tests for function fac() from mathlib, factorial from negative number is not defiend
+# @param
+# @return
 def test_factorial() -> None:
 	with pytest.raises(ValueError):
 		mathlib.fac(None, -1) == 0
@@ -71,6 +89,9 @@ def test_factorial() -> None:
 		None, 50) == 30414093201713378043612608166064768844377641568960512000000000000
 
 
+# @brief tests for function exp() from mathlib
+# @param
+# @return
 def test_exponentiationn() -> None:
 	assert mathlib.exp(None, 0, 0) == 1
 	assert mathlib.exp(None, 0, 1) == 0
@@ -82,6 +103,9 @@ def test_exponentiationn() -> None:
 	assert mathlib.exp(None, -1, 11) == -1
 
 
+# @brief tests for function fac() from mathlib, factorial from negative number is not defiend
+# @param
+# @return
 def test_nth_root() -> None:
 	assert mathlib.root(None, 2, 0) == 0
 	assert mathlib.root(None, 2, 1) == 1
@@ -90,3 +114,6 @@ def test_nth_root() -> None:
 	with pytest.raises(ValueError):
 		mathlib.root(None, -1, 1)
 		mathlib.root(None, -1, -2)
+
+
+### End of file test.py ###
