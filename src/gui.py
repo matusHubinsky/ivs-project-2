13dfe7176889ead4eaa4af41d0af65575235be9a
Adam Ďurica <adamdurica2014@gmail.com>
2023-04-03 21:39:10 +0200

    feat(gui): added buttons for brackets and help menu bar

--- src/gui.py
+++ src/gui.py
@@ -1,11 +1,25 @@
 import os
 from tkinter import *
-import tkinter as tk
+from tkinter import messagebox
 import core
 
+
 # Create the main window
-window = tk.Tk()
+window = Tk()
 window.title("Softhorn Calculator")
+
+# Create a menu bar
+menu_bar = Menu(window, background='#ADAEB3')
+
+# Create a Help menu
+help_menu = Menu(menu_bar, tearoff=0)
+help_menu.add_command(label="About", command=lambda: messagebox.showinfo("About", "Softhorn Calculator\nVersion 1.0\n\nThis calculator was created by Softhorn team."))
+help_menu.add_command(label="Commands", command=lambda: messagebox.showinfo("Commands", "Available commands:\n\n" + "\n".join(COMMANDS)))
+
+menu_bar.add_cascade(label="Help", menu=help_menu)
+
+# Display the menu bar
+window.config(menu=menu_bar)
 
 
 # Define the function to move the window with cursor
@@ -32,7 +46,7 @@ window.iconphoto(False, icon)
 
 
 # Create an Entry widget to display the input and output
-entry = tk.Entry(window, width=18, font=("Arial", 28), bg='grey90')
+entry = Entry(window, width=18, font=("Arial", 28), bg='grey90')
 entry.grid(row=1, column=0, columnspan=4, padx=5, pady=20, ipady=25)
 
 def on_enter(button):
@@ -67,76 +81,96 @@ def on_leave_top(button):
 
 def calculate_result():
     tmp = core.calculate(entry.get())
-    entry.delete(0, tk.END)
-    entry.insert(tk.END, tmp)
-    
+    entry.delete(0, END)
+    entry.insert(END, tmp)
+
+def negate_number():
+    current_value = entry.get()
+    if current_value.startswith('-'):
+        entry.delete(0, 1)  # delete negative sign
+    else:
+        entry.insert(0, '-')  # insert negative sign
+
 # Create the buttons
-button_1 = tk.Button(window, text="1", width=5, font=("Arial", 19), height=1, fg = 'white', bg='#898C96',
-                     relief='ridge', bd=3, command=lambda: entry.insert(tk.END, "1"))
-button_2 = tk.Button(window, text="2", width=5, font=("Arial", 19), height=1, fg = 'white', bg='#898C96',
-                     relief='ridge', bd=3, command=lambda: entry.insert(tk.END, "2"))
-button_3 = tk.Button(window, text="3", width=5, font=("Arial", 19), height=1, fg = 'white', bg='#898C96',
-                     relief='ridge', bd=3, command=lambda: entry.insert(tk.END, "3"))
-button_4 = tk.Button(window, text="4", width=5, font=("Arial", 19), height=1, fg = 'white', bg='#898C96',
-                     relief='ridge', bd=3, command=lambda: entry.insert(tk.END, "4"))
-button_5 = tk.Button(window, text="5", width=5, font=("Arial", 19), height=1, fg = 'white', bg='#898C96',
-                     relief='ridge', bd=3, command=lambda: entry.insert(tk.END, "5"))
-button_6 = tk.Button(window, text="6", width=5, font=("Arial", 19), height=1, fg = 'white', bg='#898C96',
-                     relief='ridge', bd=3, command=lambda: entry.insert(tk.END, "6"))
-button_7 = tk.Button(window, text="7", width=5, font=("Arial", 19), height=1, fg = 'white', bg='#898C96',
-                     relief='ridge', bd=3, command=lambda: entry.insert(tk.END, "7"))
-button_8 = tk.Button(window, text="8", width=5, font=("Arial", 19), height=1, fg = 'white', bg='#898C96',
-                     relief='ridge', bd=3, command=lambda: entry.insert(tk.END, "8"))
-button_9 = tk.Button(window, text="9", width=5, font=("Arial", 19), height=1, fg = 'white', bg='#898C96',
-                     relief='ridge', bd=3, command=lambda: entry.insert(tk.END, "9"))
-button_0 = tk.Button(window, text="0", width=5, font=("Arial", 19), height=1, fg = 'white', bg='#898C96',
-                     relief='ridge', bd=3, command=lambda: entry.insert(tk.END, "0"))
-button_plus = tk.Button(window, text="+", width=5, font=("Arial", 19), height=1, relief='ridge', fg = 'white',
-                        bd=3, bg='#898C96', command=lambda: entry.insert(tk.END, "+"))
-button_minus = tk.Button(window, text="-", width=5, height=1, relief='ridge', fg = 'white', font=("Arial", 19),
-                         bd=3, bg='#898C96', command=lambda: entry.insert(tk.END, "-"))
-button_multiply = tk.Button(window, text="*", width=5, font=("Arial", 19), height=1, relief='ridge', fg = 'white',
-                            bd=3, bg='#898C96', command=lambda: entry.insert(tk.END, "*"))
-button_divide = tk.Button(window, text="/", width=5, font=("Arial", 19), height=1, relief='ridge', fg = 'white',
-                          bd=3, bg='#898C96', command=lambda: entry.insert(tk.END, "/"))
-button_root = tk.Button(window, text=chr(0x221A), width=5, font=("Arial", 19), height=1, relief='ridge', fg = 'white',
-                        bd=3, bg='#898C96', command=lambda: entry.insert(tk.END, chr(0x221A)))
-button_power = tk.Button(window, text="pow", width=5, font=("Arial", 19), height=1, relief='ridge', fg = 'white',
-                         bd=3, bg='#898C96', command=lambda: entry.insert(tk.END, "^"))
-button_faktorial = tk.Button(window, text="!", width=5, height=1, relief='ridge', fg = 'white', font=("Arial", 19),
-                             bd=3, bg='#898C96', command=lambda: entry.insert(tk.END, "!"))
-button_neg = tk.Button(window, text="neg", width=5, fg = 'white', font=("Arial", 19),
-                       height=1, relief='ridge', bd=3, bg='#898C96')
-button_clear = tk.Button(window, text="C", width=5, font=("Arial", 19), height=1, relief='ridge', fg = 'white',
-                         bd=3, bg='#A7CCFC', command=lambda: entry.delete(0, tk.END))
-button_equal = tk.Button(window, text="=", width=5, fg = 'black', font=("Arial", 19),
+
+button_1 = Button(window, text="1", width=5, font=("Arial", 19), height=1, fg = 'white', bg='#898C96',
+                     relief='ridge', bd=3, command=lambda: entry.insert(END, "1"))
+button_2 = Button(window, text="2", width=5, font=("Arial", 19), height=1, fg = 'white', bg='#898C96',
+                     relief='ridge', bd=3, command=lambda: entry.insert(END, "2"))
+button_3 = Button(window, text="3", width=5, font=("Arial", 19), height=1, fg = 'white', bg='#898C96',
+                     relief='ridge', bd=3, command=lambda: entry.insert(END, "3"))
+button_4 = Button(window, text="4", width=5, font=("Arial", 19), height=1, fg = 'white', bg='#898C96',
+                     relief='ridge', bd=3, command=lambda: entry.insert(END, "4"))
+button_5 = Button(window, text="5", width=5, font=("Arial", 19), height=1, fg = 'white', bg='#898C96',
+                     relief='ridge', bd=3, command=lambda: entry.insert(END, "5"))
+button_6 = Button(window, text="6", width=5, font=("Arial", 19), height=1, fg = 'white', bg='#898C96',
+                     relief='ridge', bd=3, command=lambda: entry.insert(END, "6"))
+button_7 = Button(window, text="7", width=5, font=("Arial", 19), height=1, fg = 'white', bg='#898C96',
+                     relief='ridge', bd=3, command=lambda: entry.insert(END, "7"))
+button_8 = Button(window, text="8", width=5, font=("Arial", 19), height=1, fg = 'white', bg='#898C96',
+                     relief='ridge', bd=3, command=lambda: entry.insert(END, "8"))
+button_9 = Button(window, text="9", width=5, font=("Arial", 19), height=1, fg = 'white', bg='#898C96',
+                     relief='ridge', bd=3, command=lambda: entry.insert(END, "9"))
+button_0 = Button(window, text="0", width=5, font=("Arial", 19), height=1, fg = 'white', bg='#898C96',
+                     relief='ridge', bd=3, command=lambda: entry.insert(END, "0"))
+button_plus = Button(window, text="+", width=5, font=("Arial", 19), height=1, relief='ridge', fg = 'white',
+                        bd=3, bg='#898C96', command=lambda: entry.insert(END, "+"))
+button_neg = Button(window, text="±", width=5, font=("Arial", 19), height=1, relief='ridge', fg='white',
+                        bd=3, bg='#898C96', command=negate_number)
+button_minus = Button(window, text="-", width=5, height=1, relief='ridge', fg = 'white', font=("Arial", 19),
+                         bd=3, bg='#898C96', command=lambda: entry.insert(END, "-"))
+button_multiply = Button(window, text="*", width=5, font=("Arial", 19), height=1, relief='ridge', fg = 'white',
+                            bd=3, bg='#898C96', command=lambda: entry.insert(END, "*"))
+button_divide = Button(window, text="/", width=5, font=("Arial", 19), height=1, relief='ridge', fg = 'white',
+                          bd=3, bg='#898C96', command=lambda: entry.insert(END, "/"))
+button_root = Button(window, text=chr(0x221A), width=5, font=("Arial", 19), height=1, relief='ridge', fg = 'white',
+                        bd=3, bg='#898C96', command=lambda: entry.insert(END, chr(0x221A)))
+button_power = Button(window, text="x\u207F", width=5, font=("Arial", 19), height=1, relief='ridge', fg = 'white',
+                         bd=3, bg='#898C96', command=lambda: entry.insert(END, "^"))
+button_faktorial = Button(window, text="!", width=5, height=1, relief='ridge', fg = 'white', font=("Arial", 19),
+                             bd=3, bg='#898C96', command=lambda: entry.insert(END, "!"))
+button_clear = Button(window, text="C", width=5, font=("Arial", 19), height=1, relief='ridge', fg = 'white',
+                         bd=3, bg='#A7CCFC', command=lambda: entry.delete(0, END))
+button_equal = Button(window, text="=", width=5, fg = 'black', font=("Arial", 19),
                          height=1, relief='ridge', bd=3, bg='yellow', command=calculate_result)
+button_left_bracket = Button(window, text="(", width=5, font=("Arial", 19), height=1, fg = 'white', bg='#898C96',
+                     relief='ridge', bd=3, command=lambda: entry.insert(END, "("))
+button_rigth_bracket = Button(window, text=")", width=5, font=("Arial", 19), height=1, fg = 'white', bg='#898C96',
+                     relief='ridge', bd=3, command=lambda: entry.insert(END, ")"))
+button_backspace = Button(window, text="⌫", width=5, font=("Arial", 19), height=1, fg='white', bg='#898C96',
+                     relief='ridge', bd=3, command=lambda: entry.delete(len(entry.get())-1, END))
+
+
 
 # Add the buttons to the window\
-button_root.grid(row=2, column=0)
-button_power.grid(row=2, column=1)
-button_faktorial.grid(row=2, column=2)
-button_divide.grid(row=2, column=3)
+button_left_bracket.grid(row=2, column=0)
+button_rigth_bracket.grid(row=2, column=1)
+button_backspace.grid(row=2, column=3)
 
-button_1.grid(row=3, column=0)
-button_2.grid(row=3, column=1)
-button_3.grid(row=3, column=2)
-button_plus.grid(row=3, column=3)
+button_root.grid(row=3, column=0)
+button_power.grid(row=3, column=1)
+button_faktorial.grid(row=3, column=2)
+button_divide.grid(row=3, column=3)
 
-button_4.grid(row=4, column=0)
-button_5.grid(row=4, column=1)
-button_6.grid(row=4, column=2)
-button_minus.grid(row=4, column=3)
+button_1.grid(row=4, column=0)
+button_2.grid(row=4, column=1)
+button_3.grid(row=4, column=2)
+button_plus.grid(row=4, column=3)
 
-button_7.grid(row=5, column=0)
-button_8.grid(row=5, column=1)
-button_9.grid(row=5, column=2)
-button_multiply.grid(row=5, column=3)
+button_4.grid(row=5, column=0)
+button_5.grid(row=5, column=1)
+button_6.grid(row=5, column=2)
+button_minus.grid(row=5, column=3)
 
-button_neg.grid(row=6, column=0)
-button_0.grid(row=6, column=1)
-button_clear.grid(row=6, column=2)
-button_equal.grid(row=6, column=3)
+button_7.grid(row=6, column=0)
+button_8.grid(row=6, column=1)
+button_9.grid(row=6, column=2)
+button_multiply.grid(row=6, column=3)
+
+button_neg.grid(row=7, column=0)
+button_0.grid(row=7, column=1)
+button_clear.grid(row=7, column=2)
+button_equal.grid(row=7, column=3)
 
 button_1.bind("<Enter>", lambda event, button=button_1: on_enter(button))
 button_1.bind("<Leave>", lambda event, button=button_1: on_leave(button))
@@ -198,6 +232,24 @@ button_clear.bind("<Leave>", lambda event, button=button_clear: on_leave_c(butto
 button_equal.bind("<Enter>", lambda event, button=button_equal: on_enter_equal(button))
 button_equal.bind("<Leave>", lambda event, button=button_equal: on_leave_equal(button))
 
+button_left_bracket.bind("<Enter>", lambda event, button=button_left_bracket: on_enter_top(button))
+button_left_bracket.bind("<Leave>", lambda event, button=button_left_bracket: on_leave_top(button))
+
+button_rigth_bracket.bind("<Enter>", lambda event, button=button_rigth_bracket: on_enter_top(button))
+button_rigth_bracket.bind("<Leave>", lambda event, button=button_rigth_bracket: on_leave_top(button))
+
+button_backspace.bind("<Enter>", lambda event, button=button_backspace: on_enter_top(button))
+button_backspace.bind("<Leave>", lambda event, button=button_backspace: on_leave_top(button))
+
+# Define the commands available in the calculator
+COMMANDS = [    "1 ~ Inserts number 1", "2 ~ Inserts number 2", "3 ~ Inserts number 3", "4 ~ Inserts number 4",
+                "5 ~ Inserts number 5", "6 ~ Inserts number 6", "7 ~ Inserts number 7", "8 ~ Inserts number 8",
+                "9 ~ Inserts number 9", "0 ~ Inserts number 0", "+ ~ Inserts sign +",
+                "- ~ Inserts sign -", "* ~ Inserts sign *", "/ ~ Inserts sign /", chr(0x221A)+
+                " ~ Insert the number for root then insert the degree of the root", 'x\u207F ~ Insert number raised to the power n',
+                "! ~ Calculates the factorial of the inserted number", "C ~ Clears the inserted numbers and signes",
+                "⌫ ~ Deletes the last inserted character", "( ~ Inserts left bracket", ") ~ Inserts right bracket", 
+                "± ~ Negates the FISRT inserted number", "= ~ Calculates the result"]
 
 # Bind the function to the window so that it can be moved with the cursor
 entry.bind("<Button-1>", start_drag)
