5414f4ebddd645231af6780d5181fe5ea846e7e0
Matúš Hubinský <matus.hubinsky@scot.sk>
2023-04-12 13:08:24 +0200

    feat(mathlib, test): natural logarithms

--- src/mathlib.py
+++ src/mathlib.py
@@ -13,9 +13,9 @@
 
 
 # @brief calculate addition of two numbers
-# @param a 
+# @param a
 # @param b
-# @return
+# @return a plus b
 def add(tree, a, b,) -> int:
 	return a + b
 
@@ -23,7 +23,7 @@ def add(tree, a, b,) -> int:
 # @brief calculate substract of two numbers
 # @param a
 # @param b
-# @return 
+# @return a minus b
 def sub(tree, a, b) -> int:
 	if (a < 0 and b < 0):
 		return -(abs(a) + abs(b))
@@ -33,7 +33,7 @@ def sub(tree, a, b) -> int:
 # @brief multiply two numbers
 # @param a
 # @param b
-# @return 
+# @return a multiplied by b
 def mul(tree, a, b) -> int:
 	return a * b
 
@@ -41,52 +41,64 @@ def mul(tree, a, b) -> int:
 # @brief divade two numbers
 # @param a
 # @param b, can't be zero
-# @return 
+# @return a divided by b
 def div(tree, a, b) -> int:
 	if (b == 0):
 		raise ZeroDivisionError('Error: division by zero')
 	return a / b
 
 
-# @brief calculate resoult a to b power
-# @param a
+# @brief calculate resoult of a @a to @b power
+# @param a, bae
 # @param b, exponent
-# @return 
+# @return a^b
 def exp(tree, a, b) -> None:
 	return a ** b
 
 
 # @brief calculate factorial of a number, factorial of 0 is 1
 # @param a
-# @return 
+# @return factorial of a number
 def fac(tree, a) -> None:
 	# error handling
 	if (a < 0):
-		raise ValueError("Error: can't calculate factorial of a negative number")	
- 	# calculate factorial
+		raise ValueError(
+			"Error: can't calculate factorial of a negative number")
+	# calculate factorial
 	if ((a == 1) or (a == 0)):
 		return 1
 	return a * fac(None, a - 1)
 
 
-# @brief calculate n rooth from nuber x
+# @brief calculate n rooth from number x
 # @param x
 # @param n
-# @return 
+# @return n rooth from number x
 def root(tree, n, x) -> None:
 	if (x == 0 or x == 1):
 		return x
 	if (n == 1):
 		raise ValueError('Error: root must be more than one')
 	if (n < 0):
-		raise ValueError('Error: negative root is not defied')
-	if ((x < 0) and (n%2 == 0)):
+		raise ValueError('Error: negative root is not defiend')
+	if ((x < 0) and (n % 2 == 0)):
 		raise ValueError("Error: cant calculate odd root of negative number")
 	return x**(1/n)
 
 
-# @brief negate a 
+# @brief calculate negation of a
 # @param a
-# @return negation of a number
+# @return negation of number a  
 def neg(tree, a) -> None:
 	return -a
+
+
+# @brief calculate a natural logarithm
+# @param a
+# @return natural logarithm of a rounded to 6 digits
+def ln(tree, a):
+	if a <= 0:
+		raise ValueError(
+			'Error: natural logaritm of a negative number is not defiend')
+	n = 1000000.0
+	return round(n * ((a ** (1/n)) - 1), 6)
--- src/test.py
+++ src/test.py
@@ -3,9 +3,9 @@
 #
 ###############################################################################
 #
-# @file test.py 
+# @file test.py
 # @brief Test for core.py and mathlib.py
-#	
+#
 # @date 02.04.2023
 # @author Matúš Hubinský
 #
@@ -19,8 +19,8 @@ import mathlib
 
 # TODO: test for priority
 # @brief tests for the whole module calculate
-# @param
-# @return
+# @param None
+# @return None
 def test_calculate() -> None:
 	assert core.calculate("1+2") == 3
 	assert core.calculate("1/2") == 0.5
@@ -30,8 +30,8 @@ def test_calculate() -> None:
 
 
 # @brief tests for function add() from mathlib
-# @param
-# @return
+# @param None
+# @return None
 def test_addition() -> None:
 	assert mathlib.add(None, 0, 0) == 0
 	assert mathlib.add(None, 1, 2) == 3
@@ -40,8 +40,8 @@ def test_addition() -> None:
 
 
 # @brief tests for function sub() from mathlib
-# @param
-# @return
+# @param None
+# @return None
 def test_substraction() -> None:
 	assert mathlib.sub(None, 0, 0) == 0
 	assert mathlib.sub(None, 1, 2) == -1
@@ -52,19 +52,19 @@ def test_substraction() -> None:
 
 
 # @brief tests for function mul() from mathlib
-# @param
-# @return
+# @param None
+# @return None
 def test_multiplication() -> None:
 	assert mathlib.mul(None, 0, 0) == 0
 	assert mathlib.mul(None, 1, 1) == 1
 	assert mathlib.mul(None, 1, 2) == 2
 	assert mathlib.mul(None, 2, 1) == 2
 	assert mathlib.mul(None, 12345678901, 1) == 12345678901
- 
+
 
 # @brief tests for function div() from mathlib, divison by zero will return error
-# @param
-# @return
+# @param None
+# @return None
 def test_divison() -> None:
 	with pytest.raises(ZeroDivisionError):
 		mathlib.div(None, 1, 0) == 0
@@ -75,8 +75,8 @@ def test_divison() -> None:
 
 
 # @brief tests for function fac() from mathlib, factorial from negative number is not defiend
-# @param
-# @return
+# @param None
+# @return None
 def test_factorial() -> None:
 	with pytest.raises(ValueError):
 		mathlib.fac(None, -1) == 0
@@ -90,8 +90,8 @@ def test_factorial() -> None:
 
 
 # @brief tests for function exp() from mathlib
-# @param
-# @return
+# @param None
+# @return None
 def test_exponentiationn() -> None:
 	assert mathlib.exp(None, 0, 0) == 1
 	assert mathlib.exp(None, 0, 1) == 0
@@ -104,8 +104,8 @@ def test_exponentiationn() -> None:
 
 
 # @brief tests for function fac() from mathlib, factorial from negative number is not defiend
-# @param
-# @return
+# @param None
+# @return None
 def test_nth_root() -> None:
 	assert mathlib.root(None, 2, 0) == 0
 	assert mathlib.root(None, 2, 1) == 1
@@ -115,5 +115,14 @@ def test_nth_root() -> None:
 		mathlib.root(None, -1, 1)
 		mathlib.root(None, -1, -2)
 
+# @brief tests for function ln() from mathlib, natural logarithm from negative number is not defiend
+# @param None
+# @return None
+def test_natural_logarithm() -> None:
+	assert mathlib.ln(None, 1) == 0
+	assert mathlib.ln(None, 2) == 0.693147
+	assert mathlib.ln(None, 2.718281828459) == 1
+	with pytest.raises(ValueError):
+		mathlib.ln(None, -1)
 
 ### End of file test.py ###
